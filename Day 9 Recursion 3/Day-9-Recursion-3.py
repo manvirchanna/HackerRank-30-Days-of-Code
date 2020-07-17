bb7668c23d69a4ca55b2761ed17a730f8e9f6eae
Manvir <manvirsingh098@gmail.com>
2020-07-17 10:50:48 +0530

    Day 9 Recursion 3

---
+++ Day 9 Recursion 3/Day-9-Recursion-3.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the factorial function below.
+def factorial(n):
+    fact=1
+    if n==1 or n==0:
+        return 1
+    else:
+        while (n!=1):
+            fact*=n
+            n-=1
+        return fact
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
