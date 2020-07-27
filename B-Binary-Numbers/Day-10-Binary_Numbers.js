9a7bc95b5635cba7f8a6f04a7484b3fb8af2fe4f
Manvir <manvirsingh098@gmail.com>
2020-07-27 21:05:10 +0530

    Day 10 Binary Numbers

--- B-Binary-Numbers/Day-10-Binary_Numbers.js
+++ B-Binary-Numbers/Day-10-Binary_Numbers.js
@@ -1,23 +1,52 @@
-#!/bin/python3
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
 
-import math
-import os
-import random
-import re
-import sys
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
 
 
+function main() {
+    var n = parseInt(readLine());
+    var arr = [];
+    
+    while(n != 0){
+        if (n%2 == 0)
+            arr.push(0);
+        if (n%2 == 1)
+            arr.push(1);
+        n = Math.floor(n/2);
+    }
+    
+    var max = 0;
+    var cur = 0;
 
-if __name__ == '__main__':
-    num = int(input())
-    result = 0
-    maximum = 0
-    while num > 0:
-        if num % 2 == 1:
-            result += 1
-            if result > maximum:
-                maximum = result
-        else:
-            result = 0
-        num //= 2
-    print(maximum)
+    
+    for (var i = 0, len = arr.length; i < len; i++) {
+        if (arr[i] == 1)
+            cur++;
+        else if (arr[i] == 0){
+            if (cur > max)
+                max = cur;
+            cur = 0;
+        }
+    }
+    
+    if (cur > max)
+        max = cur;
+    
+    console.log(max);
+}
