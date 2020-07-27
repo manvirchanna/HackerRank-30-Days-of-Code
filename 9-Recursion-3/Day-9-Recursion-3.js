b989ddfdce378898cd02a926305835e6ec5eb919
Manvir <manvirsingh098@gmail.com>
2020-07-27 21:04:02 +0530

    Day 9 Recursion

---
+++ 9-Recursion-3/Day-9-Recursion-3.js
@@ -0,0 +1,22 @@
+function processData(input) {
+    //Enter your code here
+    console.log(factorial(input));
+}
+
+function factorial(n){
+    if (n == 0)
+        return 1;
+    else
+        return (n*(factorial(n - 1)));
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
