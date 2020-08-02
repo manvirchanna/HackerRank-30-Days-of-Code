060ccf11873c7c2f07ead46a75550f6f0e612339
Manvir <manvirsingh098@gmail.com>
2020-08-02 11:17:56 +0530

    Day 26 Nested Logic

---
+++ R-Nested Logic/Day-26-Nested-Logic.js
@@ -0,0 +1,31 @@
+function processData(input) {
+
+    var dates = input.split("\n");
+    var a = dates[0].split(" ").map(Number);
+    var b = dates[1].split(" ").map(Number);
+    var actDate = new Date( a[2] , a[1] , a[0] );
+    var expDate = new Date( b[2] , b[1] , b[0] );
+    var fine = 0
+    
+    if ( actDate <= expDate )
+        fine = 0;
+    else if ( a[1] == b[1]  &&  a[2] == b[2] )
+        fine = 15*(a[0] - b[0]);
+    else if (a[2] == b[2])
+        fine = 500*(a[1] - b[1]);
+    else
+        fine = 10000;
+    
+    console.log(fine);
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
